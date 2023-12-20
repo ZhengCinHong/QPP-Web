84be1657b0a2ad803a1db0cb4c846f0b6df3166c
Bear <a.9876542@hotmail.com>
2023-12-20 16:48:54 +0800

    修正 網址更新

--- lib/utils/url_generator.dart
+++ lib/utils/url_generator.dart
@@ -3,11 +3,11 @@ class UrlGenerator {
   /// QR Code 及按鈕 網址處理
   static String getQRCodeUrl(String url) {
     // 先檢查參數
-    // var result = nftTypeCheck(url);
-    if (url.contains('action=')) {
-      return url;
+    var result = nftTypeCheck(url);
+    if (result.contains('action=')) {
+      return result;
     }
-    Uri origin = Uri.parse(url);
+    Uri origin = Uri.parse(result);
     Map<String, String> params = Map.from(origin.queryParameters);
     // TODO: 目前強制加入 testing = true
     params.addAll({'action': 'download', 'testing': 'true'});
@@ -20,5 +20,15 @@ class UrlGenerator {
       queryParameters: params,
     );
     return generated.toString();
+  }
+
+  /// 檢查 nft 的 url 參數, 強制將 nft 資訊的 type 轉回 nft_info
+  static String nftTypeCheck(String url) {
+    // 類型是一般物品 但是 帶的參數是 metadataID
+    if (url.contains('metadataID') && url.contains('commodity_info')) {
+      print(url.replaceAll('commodity_info', 'nft_info'));
+      return url.replaceAll('commodity_info', 'nft_info');
+    }
+    return url;
   }
 }
