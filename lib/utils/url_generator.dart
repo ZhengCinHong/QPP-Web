687546645f94f0ed77980bc33937ce16fc8431af
Bear <a.9876542@hotmail.com>
2024-01-17 08:50:41 +0800

    調整 string extension url 處理

--- lib/utils/url_generator.dart
+++ lib/utils/url_generator.dart
@@ -1,3 +1,4 @@
+import 'package:qpp_example/extension/string/url.dart';
 import 'package:qpp_example/main.dart';
 
 /// 網址工具
@@ -5,11 +6,11 @@ class UrlGenerator {
   /// QR Code 及按鈕 網址處理(打開 APP)
   static String getQRCodeUrl(String url) {
     // 先檢查參數
-    var result = nftTypeCheck(url);
+    var result = url.nftTypeCheck();
     // action 要給 download, firebase 才會連結至 app
-    var checkAction = modifyUrlParameter(result, "action", "download");
+    var checkAction = result.modifyUrlParameter("action", "download");
     // line 強制用外部瀏覽器開啟
-    var checkOpen = modifyUrlParameter(checkAction, "openExternalBrowser", "1");
+    var checkOpen = checkAction.modifyUrlParameter("openExternalBrowser", "1");
     Uri origin = Uri.parse(checkOpen);
     // host 強制給原站
     String scheme = 'https';
@@ -28,25 +29,5 @@ class UrlGenerator {
     return generated.toString();
   }
 
-  /// 檢查 nft 的 url 參數, 強制將 nft 資訊的 type 轉回 nft_info
-  static String nftTypeCheck(String url) {
-    // 類型是一般物品 但是 帶的參數是 metadataID
-    if (url.contains('metadataID') && url.contains('commodity_info')) {
-      print(url.replaceAll('commodity_info', 'nft_info'));
-      return url.replaceAll('commodity_info', 'nft_info');
-    }
-    return url;
-  }
-
-  // 修改URL参数的函数
-  static String modifyUrlParameter(
-      String url, String paramName, String paramValue) {
-    Uri uri = Uri.parse(url);
-    Map<String, String> queryParameters = Map.from(uri.queryParameters);
-    queryParameters[paramName] = paramValue;
-    // 使用replace方法创建一个新的Uri对象，其中更新了参数
-    Uri modifiedUri = uri.replace(queryParameters: queryParameters);
-    // 将Uri对象转换为字符串并返回
-    return modifiedUri.toString();
-  }
+  
 }
