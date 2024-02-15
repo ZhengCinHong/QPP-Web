19e6c0c0c0696d640cea5742105fefadcb207b91
Bear <a.9876542@hotmail.com>
2024-02-15 18:27:46 +0800

    調整 router 以修正 navigator 導向問題

--- lib/go_router/router.dart
+++ lib/go_router/router.dart
@@ -5,18 +5,18 @@ import 'package:go_router/go_router.dart';
 import 'package:qpp_example/common_ui/qpp_framework/qpp_main_framework.dart';
 import 'package:qpp_example/constants/server_const.dart';
 import 'package:qpp_example/localization/qpp_locales.dart';
-import 'package:qpp_example/page/commodity_info/view/commodity_info_body.dart'
-    deferred as commodity_info_box;
-import 'package:qpp_example/page/error_page/model/error_page_model.dart';
-import 'package:qpp_example/page/error_page/view/error_page.dart'
-    deferred as error_box;
-import 'package:qpp_example/page/home/view/home_page.dart' deferred as home_box;
 import 'package:qpp_example/page/instructions/instructions_page.dart';
 import 'package:qpp_example/page/nft_info_teach/nft_info_teach_main_frame.dart';
-import 'package:qpp_example/page/user_information/view/user_information.dart'
-    deferred as user_information_box;
 import 'package:qpp_example/universal_link/universal_link_data.dart';
 import 'package:qpp_example/utils/display_url.dart';
+import 'package:qpp_example/page/error_page/model/error_page_model.dart';
+import 'package:qpp_example/page/user_information/view/user_information.dart'
+    deferred as user_information_box;
+import 'package:qpp_example/page/home/view/home_page.dart' deferred as home_box;
+import 'package:qpp_example/page/commodity_info/view/commodity_info_body.dart'
+    deferred as commodity_info_box;
+import 'package:qpp_example/page/error_page/view/error_page.dart'
+    deferred as error_box;
 import 'package:qpp_example/page/nft_info_teach/nft_info_teach_scaffold.dart'
     deferred as nft_info_teach_box;
 
@@ -29,13 +29,13 @@ class QppGoRouter {
   static const String home = '/';
 
   /// 個人資訊頁
-  static const String information = 'information';
+  static const String information = '/information';
 
   /// 物品資訊頁
-  static const String commodityInfo = 'commodity_info';
+  static const String commodityInfo = '/commodity_info';
 
   /// 物品出示頁
-  static const String commodityWithToken = 'commodity_with_token';
+  static const String commodityWithToken = '/commodity_with_token';
 
   /// 跳轉頁
   static const String go = 'go';
@@ -55,28 +55,28 @@ class QppGoRouter {
   // App
   // -----------------------------------------------------------------------------
   /// 主頁
-  static const String app = 'app';
+  static const String app = '/app';
 
   /// 個人資訊頁
-  static const String appInformation = '$app/$information';
+  static const String appInformation = '$app$information';
 
   /// 物品資訊頁
-  static const String appCommodityInfo = '$app/$commodityInfo';
+  static const String appCommodityInfo = '$app$commodityInfo';
 
   /// 物品出示頁
-  static const String appCommodityWithToken = '$app/$commodityWithToken';
+  static const String appCommodityWithToken = '$app$commodityWithToken';
 
   /// 跳轉頁
   static const String appGo = '$app/$go';
 
   /// nft物品資訊頁(只有app有)
-  static const String nftInfo = 'nft_info';
+  static const String nftInfo = '$app/nft_info';
 
   /// 外部登入(只有app有)
-  static const String vendorLogin = 'vendor_login';
+  static const String vendorLogin = '$app/vendor_login';
   // 物品移轉
-  static const String commodityRequest = 'commodity_request';
-  static const String commodityRequestV2 = 'commodity_request_v2';
+  static const String commodityRequest = '$app/commodity_request';
+  static const String commodityRequestV2 = '$app/commodity_request_v2';
 
   /// 動態牆登入授權頁(只有app有)
   static const String loginAuth = 'login_auth';
@@ -98,48 +98,53 @@ class QppGoRouter {
     return const Locale('zh', 'TW');
   }
 
+  /// 設定語系
+  static void setLocale(BuildContext context) {
+    Locale locale = getLocaleFromPath;
+    // context 設定 locale
+    context.setLocale(locale);
+    // 更新網址列
+    DisplayUrl.updateParam('lang', locale.toString());
+  }
+
   // -----------------------------------------------------------------------------
   /// The route configuration.
   // -----------------------------------------------------------------------------
   static final GoRouter router = GoRouter(
-    // initialLocation: home,
     routes: <RouteBase>[
       GoRoute(
         // 首頁
         path: home,
         name: home,
         builder: (context, state) {
-          Locale locale = getLocaleFromPath;
-          // context 設定 locale
-          context.setLocale(locale);
-          // 更新網址列
-          DisplayUrl.updateParam('lang', locale.toString());
+          setLocale(context);
           return MainFramework(
               libFuture: home_box.loadLibrary(),
               child: (context) {
                 return home_box.HomePage();
               });
         },
-        routes: _getRouters(home) +
-            [
-              GoRoute(
-                path: app,
-                name: app,
-                builder: (context, state) => MainFramework(
-                  libFuture: home_box.loadLibrary(),
-                  child: (context) {
-                    return home_box.HomePage();
-                  },
-                ),
-                routes: appRouters + _getRouters(app),
-              )
-            ],
       ),
-      // nft教學頁
       GoRoute(
+        // 首頁 app path
+        path: app,
+        name: app,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: home_box.loadLibrary(),
+            child: (context) {
+              return home_box.HomePage();
+            },
+          );
+        },
+      ),
+      GoRoute(
+        // nft教學頁
         path: nftInfoTeach,
         name: nftInfoTeach,
         builder: (context, state) {
+          setLocale(context);
           return NFTInfoTeachPageMainFrame(
               libFuture: nft_info_teach_box.loadLibrary(),
               routerState: state,
@@ -150,110 +155,96 @@ class QppGoRouter {
               });
         },
       ),
-      // 隱私權政策頁
       GoRoute(
+        // 隱私權政策頁
         path: privacy,
         name: privacy,
-        builder: (context, state) => InstructionsPage.privacy(),
+        builder: (context, state) {
+          setLocale(context);
+          return InstructionsPage.privacy();
+        },
       ),
-      // 使用者條款頁
       GoRoute(
+        // 使用者條款頁
         path: term,
         name: term,
-        builder: (context, state) => InstructionsPage.term(),
+        builder: (context, state) {
+          setLocale(context);
+          return InstructionsPage.term();
+        },
       ),
-    ],
-    errorBuilder: (context, state) => MainFramework(
-      libFuture: error_box.loadLibrary(),
-      child: (context) {
-        return error_box.ErrorPage(
-            type: ErrorPageType.urlIsWrong, url: state.fullURL);
-      },
-    ),
-  );
-
-  // -----------------------------------------------------------------------------
-  /// App路由
-  // -----------------------------------------------------------------------------
-  static List<RouteBase> appRouters = <RouteBase>[
-    // 外部登入(只有app有)
-    GoRoute(
-      path: vendorLogin,
-      name: vendorLogin,
-      builder: (context, state) => MainFramework(
-        libFuture: error_box.loadLibrary(),
-        child: (context) {
-          return error_box.ErrorPage(
-            type: ErrorPageType.troubleshootingInstructions,
-            url: state.fullURL,
+      // only app path
+      GoRoute(
+        path: vendorLogin,
+        name: vendorLogin,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: error_box.loadLibrary(),
+            child: (context) {
+              return error_box.ErrorPage(
+                type: ErrorPageType.troubleshootingInstructions,
+                url: state.fullURL,
+              );
+            },
           );
         },
       ),
-    ),
-    // 物品移轉
-    GoRoute(
-      path: commodityRequest,
-      name: commodityRequest,
-      builder: (context, state) => MainFramework(
-        libFuture: error_box.loadLibrary(),
-        child: (context) {
-          return error_box.ErrorPage(
-            type: ErrorPageType.troubleshootingInstructions,
-            url: state.fullURL,
+      // 物品移轉
+      GoRoute(
+        path: commodityRequest,
+        name: commodityRequest,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: error_box.loadLibrary(),
+            child: (context) {
+              return error_box.ErrorPage(
+                type: ErrorPageType.troubleshootingInstructions,
+                url: state.fullURL,
+              );
+            },
           );
         },
       ),
-    ),
-    // 物品移轉
-    GoRoute(
-      path: commodityRequestV2,
-      name: commodityRequestV2,
-      builder: (context, state) => MainFramework(
-        libFuture: error_box.loadLibrary(),
-        child: (context) {
-          return error_box.ErrorPage(
-            type: ErrorPageType.troubleshootingInstructions,
-            url: state.fullURL,
+      // 物品移轉
+      GoRoute(
+        path: commodityRequestV2,
+        name: commodityRequestV2,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: error_box.loadLibrary(),
+            child: (context) {
+              return error_box.ErrorPage(
+                type: ErrorPageType.troubleshootingInstructions,
+                url: state.fullURL,
+              );
+            },
           );
         },
       ),
-    ),
-    // nft物品資訊頁(只有app有)
-    GoRoute(
-      path: nftInfo,
-      name: nftInfo,
-      builder: (context, state) => MainFramework(
-        libFuture: commodity_info_box.loadLibrary(),
-        child: (context) {
-          return commodity_info_box.CommodityInfoPage(routerState: state);
+      // nft物品資訊頁(只有app有)
+      GoRoute(
+        path: nftInfo,
+        name: nftInfo,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: commodity_info_box.loadLibrary(),
+            child: (context) {
+              return commodity_info_box.CommodityInfoPage(routerState: state);
+            },
+          );
         },
       ),
-    ),
-    // 動態牆登入授權頁(只有app有)
-    // GoRoute(
-    //   path: loginAuth,
-    //   name: loginAuth,
-    //   builder: (context, state) => const MainFramework(
-    //     libFuture: ,
-    //     child: Center(
-    //       child: Text('動態牆登入授權頁'),
-    //     ),
-    //   ),
-    // ),
-  ];
-
-  // -----------------------------------------------------------------------------
-  /// 共用路徑
-  // -----------------------------------------------------------------------------
-  static List<RouteBase> _getRouters(String name) {
-    final bool isHome = name == home;
-
-    return <RouteBase>[
+      // common path
       // 個人資訊頁
       GoRoute(
         path: information,
-        name: isHome ? information : appInformation,
+        name: information,
         builder: (context, state) {
+          setLocale(context);
           final data =
               UniversalLinkParamData.fromJson(state.uri.queryParameters);
           return MainFramework(
@@ -270,36 +261,90 @@ class QppGoRouter {
       // 物品資訊頁
       GoRoute(
         path: commodityInfo,
-        name: isHome ? commodityInfo : appCommodityInfo,
-        builder: (context, state) => MainFramework(
-          libFuture: commodity_info_box.loadLibrary(),
-          child: (context) {
-            return commodity_info_box.CommodityInfoPage(routerState: state);
-          },
-        ),
+        name: commodityInfo,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: commodity_info_box.loadLibrary(),
+            child: (context) {
+              return commodity_info_box.CommodityInfoPage(routerState: state);
+            },
+          );
+        },
       ),
       // 物品出示頁
       GoRoute(
         path: commodityWithToken,
-        name: isHome ? commodityWithToken : appCommodityWithToken,
-        builder: (context, state) => MainFramework(
-          libFuture: commodity_info_box.loadLibrary(),
-          child: (context) =>
-              commodity_info_box.CommodityInfoPage(routerState: state),
-        ),
+        name: commodityWithToken,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: commodity_info_box.loadLibrary(),
+            child: (context) =>
+                commodity_info_box.CommodityInfoPage(routerState: state),
+          );
+        },
       ),
-      // 跳轉頁
-      // GoRoute(
-      //   path: go,
-      //   name: isHome ? go : appGo,
-      //   builder: (context, state) => const MainFramework(
-      //     child: Center(
-      //       child: Text('跳轉頁'),
-      //     ),
-      //   ),
-      // ),
-    ];
-  }
+      // common with app path
+      // 個人資訊頁 app path
+      GoRoute(
+        path: appInformation,
+        name: appInformation,
+        builder: (context, state) {
+          setLocale(context);
+          final data =
+              UniversalLinkParamData.fromJson(state.uri.queryParameters);
+          return MainFramework(
+            libFuture: user_information_box.loadLibrary(),
+            child: (context) {
+              return user_information_box.UserInformationOuterFrame(
+                userID: data.phoneNumber ?? "",
+                url: state.fullURL,
+              );
+            },
+          );
+        },
+      ),
+      // 物品資訊頁 app path
+      GoRoute(
+        path: appCommodityInfo,
+        name: appCommodityInfo,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: commodity_info_box.loadLibrary(),
+            child: (context) {
+              return commodity_info_box.CommodityInfoPage(routerState: state);
+            },
+          );
+        },
+      ),
+      // 物品出示頁 app path
+      GoRoute(
+        path: appCommodityWithToken,
+        name: appCommodityWithToken,
+        builder: (context, state) {
+          setLocale(context);
+          return MainFramework(
+            libFuture: commodity_info_box.loadLibrary(),
+            child: (context) =>
+                commodity_info_box.CommodityInfoPage(routerState: state),
+          );
+        },
+      ),
+    ],
+    errorBuilder: (context, state) {
+      // 錯誤頁面
+      setLocale(context);
+      return MainFramework(
+        libFuture: error_box.loadLibrary(),
+        child: (context) {
+          return error_box.ErrorPage(
+              type: ErrorPageType.urlIsWrong, url: state.fullURL);
+        },
+      );
+    },
+  );
 }
 
 extension GoRouterStateExtension on GoRouterState {
