c3d586d0049790fcffd25903dd70498654b722d6
Bear <a.9876542@hotmail.com>
2024-03-07 10:39:09 +0800

    調整產 QR Code layout padding

--- lib/common_ui/qpp_universal_link/universal_link_qrcode.dart
+++ lib/common_ui/qpp_universal_link/universal_link_qrcode.dart
@@ -22,6 +22,7 @@ class UniversalLinkQRCode extends StatelessWidget {
       children: [
         Center(
           child: Container(
+            padding: const EdgeInsets.all(11),
             decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.all(Radius.circular(6)),
@@ -57,7 +58,7 @@ class QPPQRCode extends StatelessWidget {
       embeddedImage: const AssetImage(QPPImages.pic_qrcode),
       embeddedImageStyle: const QrEmbeddedImageStyle(size: Size(30, 30)),
       size: size,
-      padding: const EdgeInsets.all(11),
+      padding: const EdgeInsets.all(0),
     );
   }
 }
